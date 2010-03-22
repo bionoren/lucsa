837f1498cb641565c439b83d27a3507b55281357
bionoren <johnboren@gmail.com>
2010-03-22 09:41:33 -0500

    refactored db initialization

--- cache.php
+++ cache.php
@@ -15,31 +15,9 @@
 
     require_once("SQLiteManager.php");
     require_once("functions.php");
+    require_once("dbinit.php");
 
     $db = new SQLiteManager("lucsa.sqlite");
-    $fields = array();
-    $fields[] = new DBField("year", DBField::NUM);
-    $db->createTable("years", $fields);
-    $fields = array();
-    $fields[] = new DBField("yearID", DBField::NUM, null, "years", "ROWID");
-    $fields[] = new DBField("id", DBField::NUM);
-    $fields[] = new DBField("name", DBField::STRING);
-    $fields[] = new DBField("acronym", DBField::STRING);
-    $fields[] = new DBField("type", DBField::NUM); //none, major, minor
-    $db->createTable("degrees", $fields);
-    $fields = array();
-    $fields[] = new DBField("degreeID", DBField::NUM, null, "degrees", "ROWID");
-    $fields[] = new DBField("department", DBField::NUM);
-    $fields[] = new DBField("departmentid", DBField::NUM);
-    $fields[] = new DBField("number", DBField::NUM);
-    $fields[] = new DBField("title", DBField::STRING);
-    $fields[] = new DBField("id", DBField::STRING);
-    $fields[] = new DBField("semester", DBField::NUM);
-    $fields[] = new DBField("offered", DBField::NUM, 3); //never, spring, fall, both
-    $fields[] = new DBField("years", DBField::NUM, 3); //never, odd, even, both
-    $fields[] = new DBField("hours", DBField::NUM, 3);
-    $fields[] = new DBField("extra", DBField::STRING);
-    $db->createTable("classes", $fields);
 
     $data = file_get_contents("http://www.letu.edu/academics/catalog/");
     $matches = array();
