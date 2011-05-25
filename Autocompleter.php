564dc51e4ded3dbfc59fb888fe3d75590ab6772e
Bion Oren <bion@serenity.(none)>
2011-05-24 22:00:40 -0500

    Autocomplete, updated roadmap

--- Autocompleter.php
+++ Autocompleter.php
@@ -2,14 +2,18 @@
     class Autocompleter {
         protected $classesTaken;
         protected $requirements;
+        protected $notes;
 
         public function __construct(ClassList $classesTaken, ClassList $requirements, Notes $notes) {
             $this->classesTaken = $classesTaken;
             $this->requirements = $requirements;
+            $this->notes = $notes;
         }
 
         public function substitute($user) {
-            $this->directSubstitutions($user);
+            $this->subFromID($user);
+            $this->subFromNotes($user);
+            $this->subFromTitle($user);
         }
 
         /**
@@ -20,45 +24,61 @@
          * @param INTEGER $user ID of the user who has taken classes.
          * @return VOID
          */
-        protected function directSubstitutions($user) {
+        protected function subFromID($user) {
             //Identical classes must be valid substitutes. Seeing as they're identical...
             foreach($this->requirements as $key=>$class) {
                 if(isset($this->classesTaken[$key])) {
                     substituteClass($user, $class->getID(), $this->classesTaken[$key]->getID());
                 }
             }
-/*                foreach($classes as $key=>$class2) {
+        }
+
+        protected function subFromNotes($user) {
+            foreach($this->requirements as $key=>$class) {
+                if($class->isComplete()) {
+                    continue;
+                }
+                foreach($this->classesTaken as $class2) {
                     if($class2->isSubstitute) {
                         continue;
                     }
-                    foreach($this->classes as $class) {
-                        if($class->isComplete()) {
-                            continue;
-                        }
-                        $note = $notes->getNote($class->getNoteID());
-                        if(!empty($note) && preg_match("/(\w{4})\s*(\d{4}).*(\w{4})\s*(\d{4})/isS", $note, $matches)) {
-                            //explicit course substitution
-                            if($class2->getDepartment() == $matches[1] && $class2->getNumber() == $matches[2]) {
-                                substituteClass($user, $class->getID(), $class2->getID());
-                                $this->completeClass($class, $class2);
-                                break;
-                            }
-                        }
-                        if($class->getNumber()) {
-                            //title & department matching (they change the middle two number sometimes, seemingly for no good reason...
-                            if($class->getDepartment() == $class2->getDepartment() && $class->getTitle() == $class2->getTitle()) {
-                                substituteClass($user, $class->getID(), $class2->getID());
-                                $this->completeClass($class, $class2);
-                                break;
-                            }
-                        }
-                        if($class->getDepartment() == $class2->getDepartment() && $class->getHours() <= $class2->getHours()) {
+                    $note = $this->notes->getNote($class->getNoteID());
+                    if(!empty($note) && preg_match("/(\w{4})\s*(\d{4}).*(\w{4})\s*(\d{4})/isS", $note, $matches)) {
+                        //explicit course substitution
+                        if($class2->getDepartment() == $matches[1] && $class2->getNumber() == $matches[2]) {
                             substituteClass($user, $class->getID(), $class2->getID());
-                            $this->completeClass($class, $class2);
                             break;
                         }
                     }
-                }*/
+                }
+            }
         }
+
+        protected function subFromTitle($user) {
+            foreach($this->requirements as $key=>$class) {
+                if($class->isComplete()) {
+                    continue;
+                }
+                foreach($this->classesTaken as $class2) {
+                    if($class2->isSubstitute) {
+                        continue;
+                    }
+                    if($class->getNumber()) {
+                        //title & department matching (they change the middle two number sometimes, seemingly for no good reason...
+                        if($class->getDepartment() == $class2->getDepartment() && $class->getTitle() == $class2->getTitle()) {
+                            substituteClass($user, $class->getID(), $class2->getID());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        /*
+        if($class->getDepartment() == $class2->getDepartment() && $class->getHours() <= $class2->getHours()) {
+            substituteClass($user, $class->getID(), $class2->getID());
+            $this->completeClass($class, $class2);
+            break;
+        }*/
     }
 ?>
